71ec6825dfc7a59dd001b9916dd3a8af43f8b5ec
Madhukar Mishra <madhukar93@gmail.com>
2023-10-13 21:06:44 +0530

    log-producer - use mocks

--- log-producer/index.js
+++ log-producer/index.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const https = require("https");
-
-// ... other parts of your code ...
+const urllib = require("url");
 
 function httpRequest(url) {
   return new Promise((resolve, reject) => {
@@ -40,43 +39,75 @@ function httpRequest(url) {
 // Entity type
 // Entity ID
 // Operation category, operation subcategory
+const mocks = {
+  success: "success",
+  internalServerError: "server-failure",
+  delayedResponse: "latency",
+  connectionReset: "connection-reset",
+  notFound: "not-found",
+  validationFailure: "validation-error",
+};
 
-exports.handler = async (event, context) => {
-  const url = process.env.TARGET_HOSTNAME;
-
-  if (!url) {
+const host = (() => {
+  console.log("API_HOST: ", process.env.API_HOST);
+  if (!process.env.API_HOST) {
     throw new Error("TARGET_URL environment variable is not set.");
   }
+  return process.env.API_HOST;
+})();
 
-  const startTime = event.startTime();
-  const response = await httpRequest(url);
-  const completionTime = Date.now() - startTime;
-
-  // Parse the JSON response
-  let responseData;
-  try {
-    responseData = JSON.parse(response);
-  } catch (error) {
-    console.error("Failed to parse JSON response:", error);
+exports.handler = async (event, context) => {
+  // base64 decode event.awslogs.data
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  console.log("context:", JSON.stringify(context, null, 2));
+  if (!event) {
+    return;
   }
+
+  const mock = mocks[event["MockScenario"]];
+
+  console.log("Mock scenario: ", mock);
+
+  if (!mock) {
+    throw new Error("Mock scenario not found");
+  }
+
+  const url = urllib.format({
+    protocol: "http",
+    host,
+    pathname: mock,
+  });
+
+  console.log("Making request to: ", url);
+
+  const startTime = Date.now();
+  let response;
+  try {
+    response = await httpRequest(url);
+    console.log("Response: ", response);
+  } catch (error) {
+    console.log("Http request error: ", error);
+  }
+  const completionTime = Date.now() - startTime;
 
   console.log({
     apiTimestamp: new Date().toISOString(),
     apiCallCompletionTime: completionTime,
     apiUrl: url,
-    responseStatusCode: responseData?.statusCode,
-    thirdPartyName: event.body.third_party_name,
-    tenantId: event.tenantId,
-    entityType: event.body.entityType,
-    entityId: event.body.entityId,
+    // 0 when there's connection failure
+    httpStatusCode: response.statusCode || 0,
+    thirdPartyName: event.ThirdPartyName,
+    tenantId: event.TenantId,
+    entityType: event.EntityType,
+    entityId: event.EntityId,
 
     // caller contains information about the invokation context like what lambda function was invoked,
     // what was the event, etc as a nested json object
     caller: {
       // maybe add some other useful information here from the api gateway event
       // avoiding nesting beyond 1 level
-      lambdaFunctionName: context.lambdaFunctionName,
-      lambdaFunctionVersion: context.lambdaFunctionVersion,
+      lambdaFunctionName: context.functionName,
+      lambdaFunctionVersion: context.functionVersion,
       lambdaFunctionArn: context.invokedFunctionArn,
       logGroupName: context.logGroupName,
       logStreamName: context.logStreamName,
